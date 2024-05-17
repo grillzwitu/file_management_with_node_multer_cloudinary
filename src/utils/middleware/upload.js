9659342f023558441900792b70410ef25c9ccdd7
Tare Appah <grillzwitu@gmail.com>
2024-05-17 13:28:08 +0100

    implemented using original file name and added date to file view

--- src/utils/middleware/upload.js
+++ src/utils/middleware/upload.js
@@ -15,7 +15,7 @@ var storage = multer.diskStorage({
     },
     filename: function(req, file, cb){
         let ext = path.extname(file.originalname)
-        cb(null, Date.now() + ext);
+        cb(null, file.originalname);
     }
 });
 
